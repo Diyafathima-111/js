6ac59320d55894993733461e18cd65fd7f47748d
Diyafathima-111 <114013839+Diyafathima-111@users.noreply.github.com>
2022-10-24 21:58:12 +0530

    Add files via upload

---
+++ index0.js
@@ -0,0 +1,18 @@
+
+
+const reactContentRoot = document.getElementById("root");
+            
+            
+            const myJSXElement = (
+                <li> Banana
+                    <li>Mango</li>
+                    <li>Grapes</li>
+                    <li>Orange</li>
+                    <li>Apple</li>
+                    <li>pineapple</li>
+                </li>
+            
+            )
+            
+             ReactDOM.render(myJSXElement,reactContentRoot)
+             
